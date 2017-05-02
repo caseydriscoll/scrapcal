865fd107f54c2071f5af5c9e73687a04d269a5f6
Casey Patrick Driscoll <caseypatrickdriscoll@me.com>
2017-05-02 14:07:15 -0400

    Add missing configuration settings for Capistano and GitLab CI

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -11,7 +11,6 @@
 set :branch, 'stage'
 set :deploy_to, "/opt/scrapcal.com/stage"
 set :deploy_via, :copy
-set :scm, :none
 
 
 
@@ -23,7 +22,7 @@ set :scm, :none
 # property set. Specify the username and a domain or IP for the server.
 # Don't use `:all`, it's a meta role.
 
-role :app, "",
+role :app, ""
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
 
