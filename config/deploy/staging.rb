f2f5f0c057154a897e1e18ffdc9aa675975d7421
Casey Patrick Driscoll <caseypatrickdriscoll@me.com>
2017-05-02 15:08:46 -0400

    Refactor to use server line for Cap file

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-# server "example.com", user: "deploy", roles: %w{app db web}
+server "127.0.0.1", user: "deploy", roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
@@ -22,7 +22,7 @@ set :deploy_via, :copy
 # property set. Specify the username and a domain or IP for the server.
 # Don't use `:all`, it's a meta role.
 
-role :app, ""
+# role :app, ""
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
 
