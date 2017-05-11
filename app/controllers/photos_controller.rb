71b2a7da9902d11abf55c554dba38d7c3beb3873
Casey Patrick Driscoll <caseypatrickdriscoll@me.com>
2017-05-11 11:52:32 -0400

    Add initial CarrierWave support

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -75,6 +75,6 @@ class PhotosController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def photo_params
-      params.require(:photo).permit(:title, :caption, :user_id, :date)
+      params.require(:photo).permit(:title, :caption, :user_id, :date, :image)
     end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,7 @@
 class Photo < ApplicationRecord
+  attr_accessor :image, :remote_image_url
+
   belongs_to :user
+
+  mount_uploader :image, ImageUploader
 end
