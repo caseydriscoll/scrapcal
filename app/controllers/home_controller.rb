370821d9defbc36ee163532a41b3c3de7c05ab59
Casey Patrick Driscoll <caseypatrickdriscoll@me.com>
2017-05-11 16:22:18 -0400

    Add initial calendar view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    date_string = params[:start_date] || DateTime.now.to_s
+    @start_date = DateTime.strptime( date_string, '%Y-%m-%d' )
+    @photos = Photo.by_month( @start_date.strftime( "%Y"), @start_date.strftime( "%m") ).all
   end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,4 +3,7 @@ class Photo < ApplicationRecord
 
   mount_uploader :image, ImageUploader
   validates_presence_of :image
+
+  scope :by_month, lambda { |year, month| where('date >= ? and date <= ?', "#{year}-#{month}-01", "#{year}-#{month}-31") }
+
 end
