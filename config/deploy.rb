3e980ac804a8715b70c1cf09e6fe11c76009aa73
Casey Patrick Driscoll <caseypatrickdriscoll@me.com>
2017-05-21 15:44:32 -0400

    Refactor deployment script to add necessary staging directories and settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :rbenv_type, :user # or :system, depends on your rbenv setup
 set :rbenv_ruby, '2.3.0'
 
 set :application, "scrapcal.com"
-set :repo_url, "ssh://git@git.caseydris.co:6753/casey/scrapcal.git"
+set :repo_url, "ssh://git/casey/scrapcal.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -27,6 +27,7 @@ set :repo_url, "ssh://git@git.caseydris.co:6753/casey/scrapcal.git"
 append :linked_files, ".env.staging", "config/database.yml", "config/secrets.yml"
 
 # Default value for linked_dirs is []
+append :linked_dirs, "public/system", "public/uploads"
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 # Default value for default_env is {}
@@ -45,12 +46,15 @@ end
 after "deploy:finished", 'deploy:say'
 
 namespace :deploy do
-  desc 'Populates the database with users after migrated'
+  desc 'Reset and populate the database with users after migrate'
   task :populate do
     on roles(:all), in: :sequence do
       within release_path do
         with rails_env: fetch(:rails_env) do
+          execute :rake, 'db:reset'
           execute :rake, 'db:populate'
+          execute "rm -rf /opt/scrapcal.com/staging/current/public/uploads/*"
+          execute "mkdir -p /opt/scrapcal.com/staging/current/public/uploads/proof/image"
         end
       end
     end
